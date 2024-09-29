3a62acba97a976682821373401d5e681cf859dad
Viranchee Lotia <hey@viranchee.com>
2024-09-29 11:13:23 -0400

    Greedy/Jumps

--- TIHandbook/Arrays/Greedy.cpp
+++ TIHandbook/Arrays/Greedy.cpp
@@ -19,5 +19,29 @@ public:
     return result;
   }
 
-  bool canJump(vector<int> &nums) {}
+  bool canJump(vector<int> &nums) {
+    int maxJ = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (i > maxJ)
+        return false;
+      maxJ = max(maxJ, i + nums[i]);
+      if (maxJ > nums.size() - 1)
+        break;
+    }
+    return true;
+  }
+
+  int jump(vector<int> &nums) {
+    int jumps = 0, near = 0, far = 0;
+    while (far < nums.size() - 1) {
+      int nextFar = far;
+      for (int i = near; i <= far; i++) {
+        nextFar = max(nextFar, i + nums[i]);
+      }
+      jumps++;
+      near = far + 1;
+      far = nextFar;
+    }
+    return jumps;
+  }
 };
